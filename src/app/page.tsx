28739b9500323e522268450b24ae2f6b8db989ad
NatalNW7 <natanaelweslley1@gmail.com>
2026-03-03 20:06:59 -0300

    feat(gifts): update bed gift details and reorder layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,8 +12,8 @@ export default function Home() {
       <Hero />
       <OurStorySection />
       <CeremonySection />
-      <RsvpSection />
       <GiftsSection />
+      <RsvpSection />
       {/* <PhotoHubSection /> */}
       <Footer />
     </>
